28adc9755ef17a10620c50693c2de740c06f0571
EdySugik <dyxzzz808@gmail.com>
2025-06-21 14:07:24 +0800

    OOP-C-pt14

---
+++ pertemuan12/src/pertemuan13/Pertemuan13.java
@@ -0,0 +1,17 @@
+package pertemuan13;
+
+/**
+ *
+ * @author Edy
+ * TGL : 24 Mei 2025
+ */
+public class Pertemuan13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
